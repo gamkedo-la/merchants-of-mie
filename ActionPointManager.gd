3fec9d1807bba15d3ea22f021fb2b4f514b8ac94
gabrielcornish <58050002+gabrielcornish@users.noreply.github.com>
2022-11-07 08:41:37 -0800

    Fixed bug with action point manager

--- ActionPointManager.gd
+++ ActionPointManager.gd
@@ -4,4 +4,4 @@ onready var action_Points_available = 5
 onready var action_Points_Per_Turn = 5 
 
 func _ready():
-	print("At turn start, player has: " + action_Points_Per_Turn + " available")
+	print("At turn start, player has: " + str(action_Points_Per_Turn) + " available")
