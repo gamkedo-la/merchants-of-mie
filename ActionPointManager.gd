a2c96227a6c2c4be50642269f0b590928ee98b9e
cubnoble <cub.noble@yahoo.fr>
2022-11-07 21:28:14 -0600

    Replacing green mesh with MerchantToken scene

---
+++ ActionPointManager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+onready var action_Points_available = 5
+onready var action_Points_Per_Turn = 5 
+
+func _ready():
+	print("At turn start, player has: " + action_Points_Per_Turn + " available")
