74f092370906b545725bf57f9794bc9703ca163a
cubnoble <cub.noble@yahoo.fr>
2022-11-11 08:29:00 -0600

    Adding Resource Spawner to GameScene. Setting up spawning through the spawner.

---
+++ ResourceSpawnManager.gd
@@ -0,0 +1,13 @@
+tool
+extends Spatial
+
+export (Array, Resource) var all_tiles
+#export var all_tiles = []
+#export var fish_tiles = []
+#export var coffee_tiles = []
+#export var gold_tiles = []
+#export var incense_tiles = []
+#export var grapes_tiles = []
+
+func _ready():
+	pass # Replace with function body.
