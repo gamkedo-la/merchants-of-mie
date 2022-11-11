bdab57ccca77a6e2a7c9c689b2af249e014527c6
cubnoble <cub.noble@yahoo.fr>
2022-11-11 07:58:10 -0600

    Resource Generator can spawn each object based on a bool

--- ResourceSpot.gd
+++ ResourceSpot.gd
@@ -1,8 +1,29 @@
 extends Node
 
-export var resource_Name = ""
+export var can_spawn_coffee = false
+export var can_spawn_fish = false
+export var can_spawn_gold = false
+export var can_spawn_grapes = false
+export var can_spawn_incense = false
 
 func _ready():
-	var scene = load("res://Tiles//ResourceScenes/Resource-Gold.tscn")
-	var resource = scene.instance()
-	add_child(resource)
+	if can_spawn_coffee:
+		var scene = load("res://Tiles//ResourceScenes/Resource-Coffee.tscn")
+		var resource = scene.instance()
+		add_child(resource)
+	if can_spawn_fish:
+		var scene = load("res://Tiles//ResourceScenes/Resource-Fish.tscn")
+		var resource = scene.instance()
+		add_child(resource)
+	if can_spawn_gold:
+		var scene = load("res://Tiles//ResourceScenes/Resource-Gold.tscn")
+		var resource = scene.instance()
+		add_child(resource)
+	if can_spawn_grapes:
+		var scene = load("res://Tiles//ResourceScenes/Resource-Grapes.tscn")
+		var resource = scene.instance()
+		add_child(resource)
+	if can_spawn_incense:
+		var scene = load("res://Tiles//ResourceScenes/Resource-Incense.tscn")
+		var resource = scene.instance()
+		add_child(resource)
