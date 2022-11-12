a9faf8ae0c87a3ac32351815633f1af3113e4d90
cubnoble <cub.noble@yahoo.fr>
2022-11-12 08:57:49 -0600

    Trying to add resources spawned to a macro variable. WIP.

--- ResourceSpawnManager.gd
+++ ResourceSpawnManager.gd
@@ -1,13 +1,3 @@
-tool
 extends Spatial
 
-export (Array, Resource) var all_tiles
-#export var all_tiles = []
-#export var fish_tiles = []
-#export var coffee_tiles = []
-#export var gold_tiles = []
-#export var incense_tiles = []
-#export var grapes_tiles = []
-
-func _ready():
-	pass # Replace with function body.
+var total_resources_on_board = 0
