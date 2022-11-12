3c97b9f27c0877c50739f526a63a78321f6f9709
cubnoble <cub.noble@yahoo.fr>
2022-11-12 08:49:43 -0600

    Resources appearing at a 10% drop rate

--- ResourceSpot.gd
+++ ResourceSpot.gd
@@ -6,24 +6,42 @@ export var can_spawn_gold = false
 export var can_spawn_grapes = false
 export var can_spawn_incense = false
 
+export var percent_chance_to_initially_drop_resource = .10
+
 func _ready():
 	if can_spawn_coffee:
-		var scene = load("res://Tiles//ResourceScenes/Resource-Coffee.tscn")
-		var resource = scene.instance()
-		add_child(resource)
+		var percent = randf()
+		print(percent)
+		if(percent > (1-percent_chance_to_initially_drop_resource)):
+			var scene = load("res://Tiles//ResourceScenes/Resource-Coffee.tscn")
+			var resource = scene.instance()
+			add_child(resource)
+			#Increase Resources on board by 1
 	if can_spawn_fish:
-		var scene = load("res://Tiles//ResourceScenes/Resource-Fish.tscn")
-		var resource = scene.instance()
-		add_child(resource)
+		var percent = randf()
+		if(percent > (1-percent_chance_to_initially_drop_resource)):
+			var scene = load("res://Tiles//ResourceScenes/Resource-Fish.tscn")
+			var resource = scene.instance()
+			add_child(resource)
+						#Increase Resources on board by 1
 	if can_spawn_gold:
-		var scene = load("res://Tiles//ResourceScenes/Resource-Gold.tscn")
-		var resource = scene.instance()
-		add_child(resource)
+		var percent = randf()
+		if(percent > (1-percent_chance_to_initially_drop_resource)):
+			var scene = load("res://Tiles//ResourceScenes/Resource-Gold.tscn")
+			var resource = scene.instance()
+			add_child(resource)
+						#Increase Resources on board by 1
 	if can_spawn_grapes:
-		var scene = load("res://Tiles//ResourceScenes/Resource-Grapes.tscn")
-		var resource = scene.instance()
-		add_child(resource)
+		var percent = randf()
+		if(percent > (1-percent_chance_to_initially_drop_resource)):
+			var scene = load("res://Tiles//ResourceScenes/Resource-Grapes.tscn")
+			var resource = scene.instance()
+			add_child(resource)
+						#Increase Resources on board by 1
 	if can_spawn_incense:
-		var scene = load("res://Tiles//ResourceScenes/Resource-Incense.tscn")
-		var resource = scene.instance()
-		add_child(resource)
+		var percent = randf()
+		if(percent > (1-percent_chance_to_initially_drop_resource)):
+			var scene = load("res://Tiles//ResourceScenes/Resource-Incense.tscn")
+			var resource = scene.instance()
+			add_child(resource)
+						#Increase Resources on board by 1
