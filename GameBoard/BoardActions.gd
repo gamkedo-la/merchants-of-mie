225910e88a15f953ef6acffbfda9839eb84a37a8
gabrielcornish <58050002+gabrielcornish@users.noreply.github.com>
2022-11-13 07:42:33 -0800

    Fixed turn structure

--- GameBoard/BoardActions.gd
+++ GameBoard/BoardActions.gd
@@ -1,5 +1,20 @@
 extends Node
 
+export var city_Name = "CITY_NAME"
+export var resource_value_silver = 3
+export var resource_value_gold = 3
+export var resource_value_salt = 3
+export var resource_value_grapes = 3
+export var resource_value_wheat = 3
+export var resource_value_tea = 3
+export var resource_value_marble = 3
+export var resource_value_incense = 3
+export var resource_value_diamonds = 3
+export var resource_value_fish = 3
+export var resource_value_lumber = 3
+export var resource_value_dyes = 3
+export var resource_value_coffee = 3
+
 signal end_turn
 
 func _ready():
