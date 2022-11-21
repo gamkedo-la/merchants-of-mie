9c1690e081ff52311b5e57c0c821f3157bbba418
cubnoble <cub.noble@yahoo.fr>
2022-11-20 18:28:07 -0600

    Adding Wheat Resource (3D and 2D)

--- ResourceSpot.gd
+++ ResourceSpot.gd
@@ -6,6 +6,7 @@ export var can_spawn_gold = false
 export var can_spawn_grapes = false
 export var can_spawn_incense = false
 export var can_spawn_salt = false
+export var can_spawn_wheat = false
 
 export var percent_chance_to_initially_drop_resource = .10
 
@@ -50,6 +51,13 @@ func _ready():
 		var percent = randf()
 		if(percent > (1-percent_chance_to_initially_drop_resource)):
 			var scene = load("res://Tiles//ResourceScenes/Resource-Salt.tscn")
+			var resource = scene.instance()
+			add_child(resource)
+			Global.total_resources_on_board += 1
+	if can_spawn_wheat:
+		var percent = randf()
+		if(percent > (1-percent_chance_to_initially_drop_resource)):
+			var scene = load("res://Tiles//ResourceScenes/Resource-Wheat.tscn")
 			var resource = scene.instance()
 			add_child(resource)
 			Global.total_resources_on_board += 1
