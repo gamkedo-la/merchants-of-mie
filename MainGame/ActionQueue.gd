0ada3a11cbe1778eef582faff7d1516a3bb25a90
cubnoble <cub.noble@yahoo.fr>
2022-12-10 20:06:57 -0600

    Adding Token Move sound effect

--- MainGame/ActionQueue.gd
+++ MainGame/ActionQueue.gd
@@ -3,6 +3,8 @@ extends Node
 var BaseAction = load("Actions/BaseAction.gd")
 var actions_list = Array()
 
+var token_move_sfx = load("Audio/SFX/TokenSlide.wav")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Events.connect("undo_actions", self, "undo")
@@ -23,5 +25,8 @@ func execute(action: BaseAction):
 	if Global.action_Points_available <= 0:
 		return
 	actions_list.append(action)
+	if !$AudioStreamPlayer3D.is_playing():
+		$AudioStreamPlayer3D.stream = token_move_sfx
+		$AudioStreamPlayer3D.play()
 	action.execute()
 	pass
