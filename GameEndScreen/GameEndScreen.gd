d06503d240419cf75e6863ab9038223c30f2517e
cubnoble <cub.noble@yahoo.fr>
2022-12-18 10:36:13 -0600

    Starting End Screen win condition

--- GameEndScreen/GameEndScreen.gd
+++ GameEndScreen/GameEndScreen.gd
@@ -2,6 +2,9 @@ extends Control
 
 func _ready():
 	Global.turn_number = 0
+	yield(get_tree().create_timer(8), "timeout")
+	for _i in self.get_children():
+		_i.is_visible_in_tree(true)
 
 func _on_ReplayBtn_pressed():
 	Global.goto_scene("res://MainGame/GameSpace.tscn")
