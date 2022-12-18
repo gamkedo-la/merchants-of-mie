afbaae464c0d6f43500e3d4e621d7dbed9db3532
cubnoble <cub.noble@yahoo.fr>
2022-12-18 17:11:05 -0600

    Winning Cutscene Created/ Working

--- GameEndScreen/GameEndScreen.gd
+++ GameEndScreen/GameEndScreen.gd
@@ -2,9 +2,8 @@ extends Control
 
 func _ready():
 	Global.turn_number = 0
-	yield(get_tree().create_timer(8), "timeout")
-	for _i in self.get_children():
-		_i.is_visible_in_tree(true)
+	yield(get_tree().create_timer(7), "timeout")
+	get_node("CenterContainer").visible = true
 
 func _on_ReplayBtn_pressed():
 	Global.goto_scene("res://MainGame/GameSpace.tscn")
