d466f2ac4f23c65293a00b0826152a5346194523
gabrielcornish <58050002+gabrielcornish@users.noreply.github.com>
2023-02-25 09:35:00 -0800

    Updated UI with new currency layout and icon

--- GUI/Currency.gd
+++ GUI/Currency.gd
@@ -5,8 +5,8 @@ func _ready():
 	if not Events.is_connected("merchant_purchased", self, "merchant_purchased"):
 		var con_res = Events.connect("merchant_purchased", self, "merchant_purchased")
 		assert(con_res == OK)
-	$VBoxContainer/CurrencyAmount.text = str(Global.currency_available)
+	$CurrencyAmount.text = str(Global.currency_available)
 
 
 func merchant_purchased():
-	$VBoxContainer/CurrencyAmount.text = str(Global.currency_available)
+	$CurrencyAmount.text = str(Global.currency_available)
